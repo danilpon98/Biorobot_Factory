67c7e2607785cd7178059864cf5d25d07cb120b7
danilpon98 <43674808+danilpon98@users.noreply.github.com>
2022-03-20 18:26:36 +0700

    config

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <img src="img/logo.png" alt="logo" />
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet
+    aperiam delectus doloribus nam numquam pariatur sed sequi sunt. Consequuntur
+    esse explicabo fuga id ipsam iste odit quibusdam sunt veniam?
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+// import app styles
+require("./scss/app.scss");
+
 createApp(App).use(store).mount("#app");
