c36e6927b0359458ed4df6ab8cb5d9bab34d8eee
danilpon98 <43674808+danilpon98@users.noreply.github.com>
2022-04-12 22:59:03 +0700

    create AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header">
+    <img src="img/logo.svg" class="logo" alt="Biorobots logo" />
+    <v-button class="header__btn" type="accent-border"
+      >Произвести биоробота</v-button
+    >
+  </header>
+</template>
+
+<script>
+import VButton from "@/components/vButton";
+export default {
+  name: "AppHeader",
+  components: { VButton },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  margin-top: 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &__btn {
+    @media (max-width: 360px) {
+      width: 100%;
+    }
+  }
+}
+.logo {
+  display: inline-block;
+  width: 78px;
+  height: auto;
+  flex-shrink: 0;
+  margin-right: 20px;
+  @media (max-width: 360px) {
+    display: none;
+  }
+}
+</style>
