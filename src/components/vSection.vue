174d1c77fbb52d1e263ffd6aefed9add02864d6a
danilpon98 <43674808+danilpon98@users.noreply.github.com>
2022-04-16 18:34:42 +0700

    vSection

---
+++ src/components/vSection.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="section">
+    <div class="section__number" v-if="numberString">{{ numberString }}</div>
+    <div class="section__scroll" v-if="scroll">Скролл</div>
+    <slot />
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "vSection",
+  props: {
+    number: {
+      type: Number,
+      required: false,
+      validate(value) {
+        return value > 0;
+      },
+    },
+    scroll: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  setup(props) {
+    const numberString = computed(() => {
+      return String(props.number).padStart(2, "0");
+    });
+
+    return {
+      numberString,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.section {
+  position: relative;
+  padding-left: 130px;
+  margin-top: 100px;
+  @include media-breakpoint-down(xl) {
+    padding-left: 0;
+  }
+  &__number,
+  &__scroll {
+    display: inline-block;
+    color: $text-grey;
+    font-weight: 500;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    font-size: 12px;
+    position: absolute;
+    @include media-breakpoint-down(xl) {
+      display: none;
+    }
+  }
+  &__number {
+    top: 20px;
+    left: 0;
+  }
+  &__scroll {
+    padding-left: 30px;
+    left: 0;
+    bottom: -10px;
+    transform: rotate(-90deg);
+    transform-origin: 0 0;
+    &::before,
+    &::after {
+      content: "";
+      display: inline-block;
+      position: absolute;
+      top: 50%;
+    }
+    &::before {
+      width: 8px;
+      height: 8px;
+      left: 0;
+      border-bottom: 1.5px solid $text-grey;
+      border-left: 1.5px solid $text-grey;
+      transform: translateY(-50%) rotate(45deg);
+      z-index: 2;
+    }
+    &::after {
+      width: 16px;
+      height: 1px;
+      left: -0.5px;
+      background-color: $text-grey;
+      transform: translateY(-50%);
+      z-index: 1;
+    }
+  }
+}
+</style>
