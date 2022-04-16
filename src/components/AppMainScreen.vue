5fe5cfd87c54a0751e0aecffe7bac6589cb08466
danilpon98 <43674808+danilpon98@users.noreply.github.com>
2022-04-16 18:35:09 +0700

    AppMainScreen

---
+++ src/components/AppMainScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-section :number="1" scroll class="overflow-hidden">
+    <div class="row gy-4 align-items-center">
+      <div class="col-sm-6 col-md-7">
+        <h1>Фабрика по производству биороботов</h1>
+        <p class="h3 text-grey mb-0 mt-4">класса «монитор-кресло»</p>
+      </div>
+      <div class="col-sm-6 col-md-5 order-first order-sm-last">
+        <img
+          class="img-responsive"
+          src="/img/main-image.png"
+          alt="Фабрика по производству биороботов"
+        />
+      </div>
+    </div>
+  </v-section>
+</template>
+
+<script>
+import VSection from "@/components/vSection";
+export default {
+  name: "AppMainScreen",
+  components: { VSection },
+};
+</script>
+
+<style scoped></style>
