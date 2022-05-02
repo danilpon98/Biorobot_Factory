da26ae304cd0cdc6d63032d84072f10c6cbec410
danilpon98 <43674808+danilpon98@users.noreply.github.com>
2022-05-02 22:50:53 +0700

    vSprite Component

---
+++ src/components/vSprite.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="['sprite', `sprite-${name}`]"></div>
+</template>
+
+<script>
+export default {
+  name: "vSprite",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/modules/sprites/sprite-robots";
+</style>
